f37e398926f7b32bd94f6e823f137dbe2b55cd17
wonik-choi <wonik.choi@susimdal.com>
2025-05-23 15:49:39 +0900

    feat: setting metadata

--- src/app/(non-certification)/layout.tsx
+++ src/app/(non-certification)/layout.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '무료체험 신청',
+};
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full w-full max-w-[45rem] mx-auto">
