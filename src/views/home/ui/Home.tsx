83650081cd0f458583a450d8956f12261803b389
wonik-choi <wonik.choi@susimdal.com>
2025-06-08 23:55:48 +0900

    fix: iframe src http -> https

--- src/views/home/ui/Home.tsx
+++ src/views/home/ui/Home.tsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <section className="flex justify-center items-center h-[calc(100vh-100px)] w-full">
       <iframe
-        src="http://dev.an2.api.slc.susimdal.com/back-office/grafana/public-dashboards/07f3e4cd2e244db889d4b9ad8c366bcc"
+        src="https://dev.an2.api.slc.susimdal.com/back-office/grafana/public-dashboards/07f3e4cd2e244db889d4b9ad8c366bcc"
         width="100%"
         height="100%"
       ></iframe>
