d4976a3d76a760efa8e75d3e192c24b26815edf7
wonik-choi <wonik.choi@susimdal.com>
2025-06-08 23:30:45 +0900

    feat: home page 외부 iframe 설정

--- src/app/(certification)/home/page.tsx
+++ src/app/(certification)/home/page.tsx
@@ -3,11 +3,7 @@ import Home from '@/views/home/ui/Home';
 export const dynamic = 'force-dynamic';
 
 const Page = () => {
-  return (
-    <section className="flex justify-center items-center h-screen w-full">
-      <iframe src="http://dev.an2.api.slc.susimdal.com/back-office/grafana" width="100%" height="100%"></iframe>
-    </section>
-  );
+  return <Home />;
 };
 
 export default Page;
