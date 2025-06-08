b8c5743f5b797448cc5dc7e93cb1299a6e6199db
wonik-choi <wonik.choi@susimdal.com>
2025-06-08 15:45:00 +0900

    feat: 대시보드 iframe 수정

--- src/app/(certification)/home/page.tsx
+++ src/app/(certification)/home/page.tsx
@@ -5,11 +5,7 @@ export const dynamic = 'force-dynamic';
 const Page = () => {
   return (
     <section className="flex justify-center items-center h-screen w-full">
-      <iframe
-        src="https://dev.an2.api.slc.susimdal.com/back-office/grafana/d-solo/aeo42id1snytce/30130850-b080-59f7-8b1e-b1ea5e9facdd?orgId=1&from=1746597769412&to=1749189769412&panelId=2"
-        width="100%"
-        height="100%"
-      ></iframe>
+      <iframe src="http://dev.an2.api.slc.susimdal.com/back-office/grafana" width="100%" height="100%"></iframe>
     </section>
   );
 };
